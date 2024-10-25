7b0168cbd2d0d8492a804fd19fd8d4984ce808e7
SoftCodeTech <softcodetech.india@gmail.com>
2024-10-26 01:34:55 +0530

    build error

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -6,17 +6,17 @@ import Link from 'next/link';
 const Breadcrumb = ({ route, breadcrumb }) => {
     return (
         <Breadcrumbs aria-label="breadcrumb" className='text-[--white]'>
-            <Link underline="hover" href="/" className='text-sm lg:text-base font-medium tracking-wider'>
+            <Link underline="hover" href="/" className='text-sm lg:text-base font-medium tracking-wider text-[--white]'>
                 HOME
             </Link>
             <Link
                 underline="hover"
                 href={route}
-                className='font-medium tracking-wider text-sm lg:text-base'
+                className='font-medium tracking-wider text-sm lg:text-base text-[--white]'
             >
                 {breadcrumb?.toUpperCase()}
-            </Link >
-        </Breadcrumbs >
+            </Link>
+        </Breadcrumbs>
     )
 }
 
